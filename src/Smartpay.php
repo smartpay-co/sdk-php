28a1b7d9c74de0f7859261c550fbabc5ec735b42
Ash Wu <hsatac@gmail.com>
2022-09-22 14:30:07 +0800

    v0.5.0
    Signed-off-by: Ash Wu <hsatac@gmail.com>

--- src/Smartpay.php
+++ src/Smartpay.php
@@ -8,7 +8,7 @@ namespace Smartpay;
 class Smartpay
 {
     const DEV_LANG = 'php';
-    const VERSION = '0.4.1';
+    const VERSION = '0.5.0';
 
     const DEFAULT_API_URL = "https://api.smartpay.co/v1";
     const DEFAULT_CHECKOUT_URL = "https://checkout.smartpay.co";
