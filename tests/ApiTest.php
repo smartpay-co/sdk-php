c495a16abcf2a4f141c94bacd5ffc4a282c15d38
Ash Wu <hsatac@gmail.com>
2023-02-06 14:14:06 +0800

    [sc-7785] Add webhook signature helpers & tests (#47)
    * Add webhook signature helpers & tests
    
    Signed-off-by: Ash Wu <hsatac@gmail.com>
    
    * Make php-cs-fixer happy
    
    Signed-off-by: Ash Wu <hsatac@gmail.com>
    
    ---------
    
    Signed-off-by: Ash Wu <hsatac@gmail.com>

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -43,4 +43,22 @@ final class ApiTest extends TestCase
         $api = new Api('sk_test_mock', 'pk_test_mock', $client);
         $this->assertSame([], $api->getOrders()->asJson());
     }
+
+    public function testCalculateWebhookSignature()
+    {
+        $api = new Api('MOCKSECRETKEY');
+        $calculatedSignature = $api->calculateWebhookSignature("test data");
+        $this->assertEquals('ae82b27dd6dafd0dbfc65faff07160833776cf60915782ef991557e51e4d1782', $calculatedSignature);
+    }
+
+    public function testValidateWebhookSignature()
+    {
+        $api = new Api('MOCKSECRETKEY');
+        $signatureValid = $api->validateWebhookSignature(
+            "test data",
+            "30f01ff4be78d2a2b053ad4a7922c4b4eb2aee75aa5326f2c9b84b52fe4e620e",
+            "test timestamp"
+        );
+        $this->assertEquals(true, $signatureValid);
+    }
 }
